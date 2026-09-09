2c429b5e9335485ea547ae409a5e58b6f0d595b1
vishnu <getvishnuvs@gmail.com>
2026-09-09 15:14:26 +0530

    feat added note controller,zod schema for validation,routes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import cookieParser from "cookie-parser";
-import express from"express";
-import cors from"cors"
+import express from "express";
+import cors from "cors";
 import { env } from "./config/env";
 import { errorHandler } from "./middleware/error-handler.middleware";
-import authRoutes from"./routes/auth.routes"
+import authRoutes from "./routes/auth.routes";
+import noteRoutes from "./routes/note.routes";
 
 const app = express();
-app.use(cors({
+
+app.use(
+  cors({
     origin: env.FROND_END_URL,
     credentials: true,
-}))
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/notes", noteRoutes);
+
 app.use(errorHandler);
-export default app
+
+export default app;
